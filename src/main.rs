3a7bb0999a9ba0f6121c9d772fd39aabaa80be13
Stepan Koltsov <stepan.koltsov@gmail.com>
2017-01-26 03:41:37 +0300

    diag

--- src/main.rs
+++ src/main.rs
@@ -10,5 +10,14 @@ extern crate regex_syntax;
 
 fn main() {
     let v: Vec<u8> = Vec::new();
+
+    // error[E0282]: unable to infer enough type information about `T`
+    //   --> src/main.rs:13:18
+    //    |
+    // 13 |     let b = v == Vec::new();
+    //    |                  ^^^^^^^^ cannot infer type for `T`
+    //    |
+    //    = note: type annotations or generic parameter binding required
+
     let b = v == Vec::new();
 }
