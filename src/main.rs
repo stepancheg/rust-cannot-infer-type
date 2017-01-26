d6ee8e9ddf5c71032960e8657576484c541f0bba
Stepan Koltsov <stepan.koltsov@gmail.com>
2017-01-26 03:25:05 +0300

    smth

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+extern crate env_logger;
+
+pub struct Payload {
+    pub xxx: ::std::vec::Vec<u8>,
+}
+
+impl Payload {
+    fn compute_size(&self) {
+        if self.xxx != ::std::vec::Vec::new() {
+        };
+    }
+}
+
+fn main() {
+}
